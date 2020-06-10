890c047a0a2735815a2d371bd78295898a37db98
Indrani Biswas <indranibiswas@Indranis-MacBook-Air.local>
2020-06-10 18:12:19 +0530

    HTML page created

--- core/views.py
+++ core/views.py
@@ -4,6 +4,8 @@ import requests
 def home(request):
     response = requests.get('http://ravigitte.pythonanywhere.com/solve/?exp=integrate(2*x%20+%20y,x)')
     data = response.json()
+    print('data', data[0]['input'])
     return render(request, 'core/home.html', {
-        'datas': data
+        'datas': data,
+        'title': data[0]['input'],
     })
